8002eaeed9b0e5d6111062f9e5f9bd66403975c2
olaa30-j <olaadel.967@gmail.com>
2025-06-25 01:23:16 +0300

    update(dropdownnotifications): notification postion

--- src/components/shared/DropdownNotification.tsx
+++ src/components/shared/DropdownNotification.tsx
@@ -106,7 +106,7 @@ const DropdownNotification = () => {
         </svg>
 
         {unreadCount > 0 && (
-          <span className="absolute left-4 top-10 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full">
+          <span className="absolute left-2 top-9 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white transform translate-x-1/2 -translate-y-1/2 bg-red-500 rounded-full">
             {unreadCount > 9 ? '9+' : unreadCount}
           </span>
         )}
