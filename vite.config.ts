c2db04d4ce3612bd2aef4e57e27a8bf37104c999
olaa30-j <olaadel.967@gmail.com>
2025-06-26 01:53:04 +0300

    update(contactus): script app sheet

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['swiper'],
+  },
   server: {
     proxy: {
-      '/api': {
+      '/google-script': {
         target: 'https://script.google.com',
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/google-script/, ''),
+        headers: {
+          "Origin": "http://localhost:5173",
+          "Access-Control-Allow-Origin": "http://localhost:5173",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization"
+        },
         configure: (proxy) => {
           proxy.on('proxyReq', (proxyReq) => {
-            proxyReq.setHeader('Origin', 'http://localhost:5173')
-            proxyReq.setHeader('Referer', 'http://localhost:5173')
-          })
+            proxyReq.setHeader('Referer', 'http://localhost:5173');
+            proxyReq.setHeader('X-Requested-With', 'XMLHttpRequest');
+          });
+          proxy.on('proxyRes', (proxyRes) => {
+            proxyRes.headers['Access-Control-Allow-Origin'] = 'http://localhost:5173';
+            proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+          });
+        }
+      },
+      '/api': {
+        target: 'https://backend-tests-delta.vercel.app',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1'),
+        headers: {
+          "x-vercel-project-id": "prj_a8J10S0lW2iSCK4mDyT3PHAYx6Lv"
         }
       }
+    },
+    cors: {
+      origin: 'http://localhost:5173',
+      methods: 'GET,POST,PUT,DELETE,OPTIONS',
+      allowedHeaders: ['Content-Type', 'Authorization'],
+      credentials: true
     }
   }
-})
+});
